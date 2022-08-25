93c91fa074c0fdd30aa00091846e03421baf572c
zisgeim 09 <ami145388@gmail.com>
2022-08-25 20:06:47 +0600

    second

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,21 @@ export default function Index() {
         .catch((e) => console.log(e));
     }, [userId])
     return (
-        <>
-            <h1>{user.name} (a.k.a {user.username})</h1>    
+        <div className='wrapper'>
+            <h1>{user.name} (a.k.a {user.username})</h1>
             <h2>Company: {user.company?.name}</h2>
             <h2>Location: {user.address?.city}, {user.address?.street}, {user.address?.suite} ({user.address?.geo.lat}, {user.address?.geo.lng})</h2>
-            <button onClick={() => setUserId(userId-1)} className={userId == 1 ? "unavailable" : "available"}>Previous</button>
-            <button onClick={() => setUserId(userId+1)} className={userId == 10 ? "unavailable" : "available"}>Next</button>
-        </>
+            <h2>Website: {user.website}</h2>
+            <span>
+                <span>
+                    <button onClick={() => setUserId(userId-1)} className={userId == 1 ? "unavailable" : "available"}>Previous</button>  
+                </span>
+                <span>{userId}</span>
+                <span>
+                <button onClick={() => setUserId(userId+1)} className={userId == 10 ? "unavailable" : "available"}>Next</button>
+                </span>
+            </span>
+            <h2>Contacts: {user.phone} or {user.email}</h2>
+        </div>
     )
 }
